d5e8d83a4273394c56e2f25adbab8f78efb2ae3f
binau <binau@binau.org>
2018-07-12 19:13:28 +0200

    Mise à jour proxy

--- src/app/service/test.runner.service.ts
+++ src/app/service/test.runner.service.ts
@@ -9,15 +9,17 @@ import {TestParam} from '../component/tests/bean/exec.api.bean';
 })
 export class TestRunnerService {
 
-  constructor(
-    private wsService: WebsocketService
-  ) {
+  constructor(private wsService: WebsocketService) {
   }
 
   public async test(param: TestParam): Promise<Observable<any>> {
-
-    const observable: Observable<string> =
-      await this.wsService.connect('ws://localhost:3333/ws/runTest');
+    let observable: Observable<string>;
+    try {
+      observable =
+        await this.wsService.connect('ws://localhost:8333/ws/runTest');
+    } catch (e) {
+      console.log(e);
+    }
 
     this.wsService.send(JSON.stringify(param));
 
